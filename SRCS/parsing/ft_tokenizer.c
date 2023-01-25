6b4b3bd053b848fef20ce88dcddaf8c20789af63
Mohammad Massarweh <mmassarw@lab2r3s3.42abudhabi.ae>
2023-01-26 01:49:36 +0400

    last commit update

--- SRCS/parsing/ft_tokenizer.c
+++ SRCS/parsing/ft_tokenizer.c
@@ -6,11 +6,19 @@
 /*   By: mmassarw <mmassarw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 23:12:20 by mmassarw          #+#    #+#             */
-/*   Updated: 2023/01/26 00:35:35 by mmassarw         ###   ########.fr       */
+/*   Updated: 2023/01/26 01:49:12 by mmassarw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+void	ft_tokenlist(t_mini *mini)
+{
+	size_t	offset;
+	t_token	head;
+	t_token	tail;
+
+}
 
 void	ft_tokenize(t_mini *mini)
 {
